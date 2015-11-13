f79b6d73e822fa9df49ee6fb96f87bd72d665e22
Anton Vdovin <antonvdovin92@gmail.com>
2015-11-13 18:34:14 +0200

    added more functions

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -15,11 +15,72 @@ numoApp.controller('mainController',['$scope', '$routeParams', '$firebaseArray',
             //$location.path("/home/event");
           });
       };
+<<<<<<< HEAD
+     //share functions
+      $scope.vk = function(eventW) {
+          console.log(eventW)
+            url  = 'http://vkontakte.ru/share.php?';
+            url += 'url='          + encodeURIComponent("http://localhost:64029/#/home/-K300UzhmbBNqksQYOrX");
+            url += '&title='       + encodeURIComponent(eventW.title);
+            url += '&description=' + encodeURIComponent(eventW.description);
+            url += '&image='       + encodeURIComponent(eventW.img_url);
+            url += '&noparse=true';
+            popup(url);
+            console.log(url);
+          };
+
+      $scope.facebook = function(eventW){
+            url  = 'http://www.facebook.com/sharer.php?s=100';
+            url += '&p[title]='     + encodeURIComponent("http://localhost:64029/#/home/-K300UzhmbBNqksQYOrX");
+            url += '&p[summary]='   + encodeURIComponent(eventW.title);
+            url += '&p[url]='       + encodeURIComponent(eventW.description);
+            url += '&p[images][0]=' + encodeURIComponent(eventW.img_url);
+            popup(url);
+      };
+
+      function popup(url) {
+            window.open(url,'','toolbar=0,status=0,width=626,height=436');
+          };
+
+/*
+      $scope.Share = {
+          vkontakte: function(purl, ptitle, pimg, text) {
+            url  = 'http://vkontakte.ru/share.php?';
+            url += 'url='          + encodeURIComponent(purl);
+            url += '&title='       + encodeURIComponent(ptitle);
+            url += '&description=' + encodeURIComponent(text);
+            url += '&image='       + encodeURIComponent(pimg);
+            url += '&noparse=true';
+            Share.popup(url);
+          },
+          facebook: function(purl, ptitle, pimg, text) {
+            url  = 'http://www.facebook.com/sharer.php?s=100';
+            url += '&p[title]='     + encodeURIComponent(ptitle);
+            url += '&p[summary]='   + encodeURIComponent(text);
+            url += '&p[url]='       + encodeURIComponent(purl);
+            url += '&p[images][0]=' + encodeURIComponent(pimg);
+            Share.popup(url);
+          },
+          twitter: function(purl, ptitle) {
+            url  = 'http://twitter.com/share?';
+            url += 'text='      + encodeURIComponent(ptitle);
+            url += '&url='      + encodeURIComponent(purl);
+            url += '&counturl=' + encodeURIComponent(purl);
+            Share.popup(url);
+          },
+          popup: function(url) {
+            window.open(url,'','toolbar=0,status=0,width=626,height=436');
+          }
+      };*/
+
+	}]);
+=======
     
 	}]);
 
 
 
+>>>>>>> ccbc0759cd87c701e65fb2148b378d19622775ea
 
 
 function initMap(){
